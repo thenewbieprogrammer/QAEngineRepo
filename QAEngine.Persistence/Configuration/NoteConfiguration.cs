6d61b779c17a3e157a39a9dae342c753d65266df
B Win10 Work <bilaalnadeem95@gmail.com>
2018-10-08 17:28:05 +0100

    See file NoteConfiguration.cs for more info

---
+++ QAEngine.Persistence/Configuration/NoteConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QAEngine.Persistence.Configuration
+{
+    // to do.. not vital at this point. 
+    // testing of Crud operation within Customer needs to occur.
+    class NoteConfiguration
+    {
+    }
+}
